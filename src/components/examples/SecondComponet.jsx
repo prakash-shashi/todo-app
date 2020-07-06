97d35dcca6ddbcb913b7455da4c2fa514f436e1c
“shashi211” <shashiprakash.srk@gmail.com>
2020-07-06 11:54:27 +0530

    playing with components

---
+++ src/components/examples/SecondComponet.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+//Class Component
+export default class SecondComponent extends Component {
+    render() {
+      return (
+        <div className="SecondComponent">
+          Second Component
+        </div>
+      );
+    }
+  }
---
+++ src/components/examples/ThirdComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+//Function Component
+export default function ThirdComponent(){
+    return (
+    <div className="thirdComponent">
+      Third Component
+    </div>
+    );
+}
